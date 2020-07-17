c3cc02c274ae8f73ad2c8d3a06409458e6daeb48
Hector Alvarez <h-alvarez@wiu.edu>
2020-07-17 03:47:28 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,12 @@ def assembled_aoh
 end
 
 def literal_aoh
-  literal_array = [ ]
+  literal_array = [
+    { name:  "Don Gately", occupation: "Live-in Staff" }
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" }
+PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
+KATE_G = { name:  "Kate Gompert", occupation: "None" }
+BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" } ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
